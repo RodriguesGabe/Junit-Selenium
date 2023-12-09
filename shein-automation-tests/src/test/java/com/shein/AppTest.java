307e9cec3c07eb7fe526a274d05ddf735ca8286f
RodriguesGabe <105085377+RodriguesGabe@users.noreply.github.com>
2023-12-09 03:40:32 +0000

    updated AppTest.java

--- shein-automation-tests/src/test/java/com/shein/AppTest.java
+++ shein-automation-tests/src/test/java/com/shein/AppTest.java
@@ -3,6 +3,8 @@ package com.shein;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
