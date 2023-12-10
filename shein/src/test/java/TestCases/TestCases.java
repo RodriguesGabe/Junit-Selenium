ee4eb6f4e13457a725000dbd4b7f9caa5e3666cd
Gitgabe <cybergabe01@gmail.com>
2023-12-09 21:49:43 -0300

    updated files and geckodriver

--- shein/src/test/java/TestCases/TestCases.java
+++ shein/src/test/java/TestCases/TestCases.java
@@ -2,33 +2,46 @@ package TestCases;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class TestCases {
 
-     @BeforeEach
+    private WebDriver driver;
+
+    @BeforeEach
     public void setup() {
-      // Abrir o navegador
-      WebDriver driver = new ChromeDriver();
+        // Configurar o WebDriver (GeckoDriver para o Firefox no seu caso)
+        System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+        
+        // Configurar as opções do Firefox (opcional)
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--headless");  // Executar em modo headless (sem interface gráfica)
 
-      // Navegar para a página de teste
-      driver.get("https://www..com/");
+        // Inicializar o WebDriver do Firefox
+        driver = new FirefoxDriver(options);
     }
-     
-     @Test
-     public void testNavegarPaginaInicial() {
-       // Abrir o navegador
-       WebDriver driver = new ChromeDriver();
 
-       // Navegar para a página inicial da SHEIN
-       driver.get("https://www..com/");
+    @Test
+    public void testNavegarPaginaInicial() {
+        // Navegar para a página inicial da SHEIN
+        driver.get("https://www.shein.com/");
 
-       // Verificar se a página inicial é exibida
-       assertEquals("12.12 Sale 2023 | Moda feminina | Roupas, Acessórios e muitos mais | SHEIN Brasil", driver.getTitle());
-     }
-     
-}    
+        // Verificar se a página inicial é exibida corretamente
+        assertEquals("12.12 Sale 2023 | Moda feminina | Roupas, Acessórios e muitos mais | SHEIN Brasil", driver.getTitle());
+    }
+
+    // Adicione métodos de teste adicionais conforme necessário
+
+    @AfterEach
+    public void tearDown() {
+        // Fechar o navegador após cada teste
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
